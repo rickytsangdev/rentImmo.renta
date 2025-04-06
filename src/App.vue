e0e3a26f1befef822ce91fc00a76ecd74f22f8a0
rickytsangdev <rickytsang.dev@gmail.com>
2025-04-06 19:18:41 +0200

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Form from './components/Form.vue'
+</script>
+
+<template>
+  <v-toolbar class="header" :elevation="8" title="RentImmo.renta"></v-toolbar>
+  <Form></Form>
+</template>
+
+<style scoped>
+.header {
+  background-color: orange;
+  color: white;
+  margin-bottom: 3%;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const firstName = ref('')
+
+const rules = [
+  (value) => {
+    if (value) return true
+    return 'You must enter a first name.'
+  },
+]
+</script>
+
+<template>
+  <v-sheet class="mx-auto" width="300">
+    <v-form @submit.prevent>
+      <v-text-field v-model="firstName" :rules="rules" label="Prix d'achat"></v-text-field>
+    </v-form>
+    <v-form @submit.prevent>
+      <v-text-field v-model="loyer" :rules="rules" label="Loyer mensuel"></v-text-field>
+    </v-form>
+    <v-form @submit.prevent>
+      <v-text-field v-model="firstName" :rules="rules" label="Charges"></v-text-field>
+    </v-form>
+    <v-form @submit.prevent>
+      <v-text-field v-model="firstName" :rules="rules" label="First name"></v-text-field>
+      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
